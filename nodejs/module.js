759d2f93d4f4d30461729e85af92dcba3a1a4592
Dilee Deepu <dileepdeepu1995@yahoo.com>
2019-08-13 15:13:54 +0530

    maths

--- nodejs/module.js
+++ nodejs/module.js
@@ -3,3 +3,17 @@ module.exports.add= (a,b)=>{
     var c=a+b;
     return c;
 }
+
+module.exports.sub= (a,b)=>{
+    var c=a-b;
+    return c;
+}
+module.exports.mult= (a,b)=>{
+    var c=a*b;
+    return c;
+}
+module.exports.div= (a,b)=>{
+    var c=a/b;
+    return c;
+}
+
